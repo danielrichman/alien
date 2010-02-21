1e5344f0be96dc029e8923a47f5e6649642fc328
Daniel Richman <main@danielrichman.co.uk>
2010-02-21 20:23:23 +0000

    * Upgraded the nmea-style checksum program. It lacks comments (sorry) but     makes up for that in functionality. Could probably do with a bit more     bug testing, but *appears* stable enough.
     -- Daniel Richman

--- misc-c/pc/nmea-checksum-example.c
+++ misc-c/pc/nmea-checksum-example.c
@@ -17,28 +17,217 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
 int main(int argc, char **argv)
 {
-  unsigned char chksum;
-  int i, len;
+  uint8_t chksum, chksum_verify, chksum_verify_status, chksum_verify_reverse, chksum_printed;
+  uint8_t option_newl, option_chkrm, option_help, options_valid;
+  size_t i;
+  uint8_t j, k, l;
+  int m;
 
-  if (argc != 2)
+  option_newl   = 1;
+  option_help   = 0;
+  option_chkrm  = 0;
+  options_valid = 1;
+
+  if (argc == 2 && argv[1][0] == '-' && argv[1][1] != '\0')
   {
-    printf("Usage: %s <string to generate checksum from>\n", argv[0]);
-    exit(-1);
+    j = strlen(argv[1]);
+
+    for (i = 1; i < j; i++)
+    {
+      if (argv[1][i] == 'n' && option_newl)
+      {
+        option_newl  = 0;
+      }
+      else if (argv[1][i] == 'h' && !option_help)
+      {
+        option_help  = 1;
+      }
+      else if (argv[1][i] == 'c' && !option_chkrm)
+      {
+        option_chkrm = 1;
+      }
+      else
+      {
+        options_valid = 0;
+        break;
+      }
+    }
+  }
+  else if (argc != 1)
+  {
+    options_valid = 0;
   }
 
-  len = strlen(argv[1]);
+  if (option_help && (!option_newl || option_chkrm))
+  {
+    options_valid = 0;
+  }
+
+  if (!options_valid)
+  {
+    fprintf(stderr, "Usage: %s [-cnh]\n", argv[0]);
+    fprintf(stderr, "Use -h for help\n");
+    return 1;
+  }
+
+  if (option_help)
+  {
+    fprintf(stderr, "Usage: %s [-cnh]\n", argv[0]);
+    fprintf(stderr, "     -h  displays this help\n");
+    fprintf(stderr, "     -n  checksum the whole file, instead of each line\n");
+    fprintf(stderr, "     -c  enables detection and validation of checksum\n");
+    return 1;
+  }
+
   chksum = 0;
+  chksum_verify = 0;
+  chksum_verify_status = 0;
+  chksum_verify_reverse = 0;
+  chksum_printed = 0;
 
-  for (i = 0; i < len; i++)
+  while (!feof(stdin))
   {
-    chksum ^= argv[1][i];    
+    m = getchar();
+
+    if (m == EOF)
+    {
+      break;
+    }
+
+    k = ((uint8_t) m);
+
+    if ((k < ' ' || k > '~') && k != '\n')
+    {
+      continue;
+    }
+
+    chksum_printed = 0;
+
+    if (k == '\n')
+    {
+      if (option_newl)
+      {
+        if (option_chkrm)
+        {
+          if (chksum_verify_status == 3)
+          {
+            chksum ^= '*';
+            chksum ^= chksum_verify_reverse;
+
+            if (chksum == chksum_verify)
+            {
+              printf("*%.2X *%.2X OK\n",  chksum, chksum_verify);
+            }
+            else
+            {
+              printf("*%.2X *%.2X BAD\n", chksum, chksum_verify);
+            }
+          }
+          else
+          {
+            printf("*%.2X MISSING\n", chksum);
+          }
+        }
+        else
+        {
+          printf("*%.2X\n", chksum);
+        }
+
+        chksum_printed = 1;
+        chksum = 0;
+        chksum_verify_status = 0;
+      }
+    }
+    else
+    {
+      if (option_chkrm)
+      {
+        if (k == '*')
+        {
+          chksum_verify_status = 1;
+        }
+        else if (chksum_verify_status == 1 || chksum_verify_status == 2)
+        {
+          if (k >= '0' && k <= '9')
+          {
+            l = k - '0';
+          }
+          else if (k >= 'a' && k <= 'f')
+          {
+            l = (k - 'a') + 10;
+          }
+          else if (k >= 'A' && k <= 'F')
+          {
+            l = (k - 'A') + 10;
+          }
+          else 
+          {
+            l = 255;
+          }
+
+          if (l != 255)
+          {
+            if (chksum_verify_status == 1)
+            {
+              chksum_verify_reverse = k;
+              chksum_verify = l << 4;
+              chksum_verify_status = 2;
+            }
+            else
+            {
+              chksum_verify_reverse ^= k;
+              chksum_verify |= l;
+              chksum_verify_status = 3;
+            }
+          }
+          else
+          {
+            chksum_verify_status = 0;
+          }
+        }
+        else if (chksum_verify_status == 3)
+        {
+          chksum_verify_status = 0;
+        }
+      }
+
+      chksum ^= k;
+    }
   }
 
-  printf("*%.2X\n", chksum);
+  if (!chksum_printed)
+  {
+    if (option_chkrm)
+    {
+      if (chksum_verify_status == 3)
+      {
+        chksum ^= '*';
+        chksum ^= chksum_verify_reverse;
+
+        if (chksum == chksum_verify)
+        {
+          printf("*%.2X *%.2X OK\n",  chksum, chksum_verify);
+        }
+        else
+        {
+          printf("*%.2X *%.2X BAD\n", chksum, chksum_verify);
+        }
+      }
+      else
+      {
+        printf("*%.2X MISSING\n", chksum);
+      }
+    }
+    else
+    {
+      printf("*%.2X\n", chksum);
+    }
+  }
 
   return 0;
 }
